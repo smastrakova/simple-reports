cdf8640e48ba1769ba998d2edb5fd803ed15b6b2
ZEPHYRUS-1 <sona.mastrakova@gmail.com>
2024-11-11 09:51:14 +0100

    Introduced Mongo connection

--- backend/src/appConfig.ts
+++ backend/src/appConfig.ts
@@ -3,3 +3,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const PORT: number = Number(process.env.PORT) || 5000
+export const DATABASE_URL: string =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017'
--- backend/src/utils/gracefulShutdown.ts
+++ backend/src/utils/gracefulShutdown.ts
@@ -1,10 +1,15 @@
 import { Server } from 'node:http'
+import { IDatabaseConnector } from '../db/IDatabaseConnector.js'
 import { close } from '../httpServer.js'
 import logger from './logger.js'
 
-const gracefulShutdown = async (server: Server): Promise<void> => {
+const gracefulShutdown = async (
+  server: Server,
+  dbConnector: IDatabaseConnector
+): Promise<void> => {
   logger.info('Shutting down gracefully...')
 
+  await dbConnector.disconnectDB()
   await close(server)
 
   logger.info('Shutdown complete. Exiting process.')
